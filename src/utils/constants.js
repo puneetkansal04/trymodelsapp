bfd862ab79f570532c373ec4e206d386cf975bd1
Puneet <48505993+puneetkansal04@users.noreply.github.com>
2024-03-30 19:05:11 +0530

    new app release

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 import {Platform} from 'react-native';
 const productSkus = Platform.select({
-  android: ['premium_plus_plan','premium_plan',],
+  android: ['premium_plan','premium_plus_plan'],
 });
 export const constants = {
   productSkus,
